8cc7c141dd2cd26daf61db0b42be5ad2f3f32372
ALI <alijonzabirov20@mail.ru>
2022-03-25 18:09:47 +0500

    creat a new file

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+#include <algorithm>
+using namespace std;
+
+int main ()
+{
+    int a,b;
+    cin >> a;
+    for ( int i = 1; i < a; i ++ )
+    {
+        cout << endl;
+        for ( int j = 1; j <= 10; j ++ )
+        {
+            cout << i <<" * " << j <<" = "<< i * j <<endl;
+        }
+    }
+
+}
